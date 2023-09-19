504e075ebc329d083698811dbdd4412a5b8c1100
Adewale Fatoki <adewaleftk@gmail.com>
2023-09-19 13:47:20 +0100

    updated

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import usePackageStore from '../../../store';
-import './dashboard.css'; // Import the CSS file
+import '../styles/dashboard.css'; // Import the CSS file
 
 const Dashboard = () => {
   const balance = usePackageStore((state) => state.balance);
