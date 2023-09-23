a281bf8bc49f7f6b7ea70ab41d9f06c5f358b81d
Adewale Fatoki <adewaleftk@gmail.com>
2023-09-23 21:06:32 +0100

    updated

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import usePackageStore from '../../../store';
-import '../styles/dashboard.css'; // Import the CSS file
+import '../styles/dashboard.css';
 
 const Dashboard = () => {
   const balance = usePackageStore((state) => state.balance);
   const logout = usePackageStore((state) => state.logout);
   const navigate = useNavigate();
+
+  // Load balance from local storage when the component mounts
+  useEffect(() => {
+    const storedBalance = localStorage.getItem('balance');
+    if (storedBalance) {
+      // Parse the stored balance as a number
+      usePackageStore.setState({ balance: parseFloat(storedBalance) });
+    }
+  }, []);
+
+  // Save the balance to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('balance', balance.toString());
+  }, [balance]);
 
   const handleDeposit = () => {
     navigate('/deposit');
