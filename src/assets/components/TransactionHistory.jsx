f144d6d517d4e96b8a698b862776120f065055c8
Adewale Fatoki <adewaleftk@gmail.com>
2023-09-25 01:06:07 +0100

    updated

--- src/assets/components/TransactionHistory.jsx
+++ src/assets/components/TransactionHistory.jsx
@@ -41,7 +41,10 @@ function TransactionHistory() {
               <p className="transaction-amount">Amount: {transaction.amount}</p>
               <p className="transaction-type">Type: {transaction.type}</p>
               <p className="transaction-time">Time: {transaction.timestamp}</p>
-              {/* Add more details from your Transaction model */}
+              {transaction.sender && (
+                <p className="transaction-time">Sender: {transaction.sender.accountNumber}</p>
+                // Add more details from your Transaction model
+              )}
             </div>
           </li>
         ))}
